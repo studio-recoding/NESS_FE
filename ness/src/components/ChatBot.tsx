15d45cda9f9c9f886a4b19436adf19f87a24b8c9
hmuri <113418916+hmuri@users.noreply.github.com>
2023-12-05 11:34:29 +0900

    feat: 기본 챗봇 설정 완료

--- ness/src/components/ChatBot.tsx
+++ ness/src/components/ChatBot.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import chatMessages from "../../public/messages.json"; // messages.json 파일 임포트
+import chatMessages from "../messages.json"; // messages.json 파일 임포트
 
 const ChatContainer = styled.div`
   // 채팅 컨테이너 스타일
@@ -8,6 +8,20 @@ const ChatContainer = styled.div`
 
 const Message = styled.div<{ sender: string }>`
   // 메시지 스타일 (sender에 따라 다름)
+`;
+
+const InputArea = styled.div`
+  margin-top: 20px;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  margin-right: 10px;
+  width: 70%;
+`;
+
+const Button = styled.button`
+  padding: 10px;
 `;
 
 interface ChatMessage {
@@ -19,15 +33,38 @@ const ChatBot: React.FC = () => {
   const [messages, setMessages] = useState<ChatMessage[]>(
     chatMessages.messages as ChatMessage[]
   );
+  const [userInput, setUserInput] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserInput(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (userInput.trim() === "") {
+      return; // 빈 입력 방지
+    }
+    const newUserMessage: ChatMessage = {
+      sender: "user",
+      content: userInput,
+    };
+    setMessages(messages.concat(newUserMessage));
+    setUserInput(""); // 입력 필드 초기화
+  };
 
   return (
-    <ChatContainer>
-      {messages.map((msg, index) => (
-        <Message key={index} sender={msg.sender}>
-          {msg.content}
-        </Message>
-      ))}
-    </ChatContainer>
+    <div>
+      <ChatContainer>
+        {messages.map((msg, index) => (
+          <Message key={index} sender={msg.sender}>
+            {msg.content}
+          </Message>
+        ))}
+      </ChatContainer>
+      <InputArea>
+        <Input type="text" value={userInput} onChange={handleInputChange} />
+        <Button onClick={handleSubmit}>Send</Button>
+      </InputArea>
+    </div>
   );
 };
 
