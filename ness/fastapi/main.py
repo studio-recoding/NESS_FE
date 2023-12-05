1fe457cd2e3a754e8a42faae42b0ade950f7d37c
hmuri <113418916+hmuri@users.noreply.github.com>
2023-12-05 11:48:38 +0900

    feat: fastapi 설정

---
+++ ness/fastapi/main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # 환경 변수 로드
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+app = FastAPI()
+
+# CORS 미들웨어 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], # React 애플리케이션이 실행되는 주소
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 HTTP 헤더 허용
+)
+
+class UserMessage(BaseModel):
+    message: str
+
+
+@app.post("/chat/")
+async def chat_with_gpt(user_message: UserMessage):
+    try:
+        response = requests.post(
+            "https://api.openai.com/v1/engines/gpt-3.5-turbo/completions",
+            headers={
+                "Authorization": f"Bearer {OPENAI_API_KEY}"
+            },
+            json={
+                "model": "gpt-3.5-turbo",
+                "prompt": user_message.message,
+                "max_tokens": 150
+            }
+        )
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- ness/src/components/ChatBot.tsx
+++ ness/src/components/ChatBot.tsx
@@ -2,28 +2,6 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import chatMessages from "../messages.json"; // messages.json 파일 임포트
 
-const ChatContainer = styled.div`
-  // 채팅 컨테이너 스타일
-`;
-
-const Message = styled.div<{ sender: string }>`
-  // 메시지 스타일 (sender에 따라 다름)
-`;
-
-const InputArea = styled.div`
-  margin-top: 20px;
-`;
-
-const Input = styled.input`
-  padding: 10px;
-  margin-right: 10px;
-  width: 70%;
-`;
-
-const Button = styled.button`
-  padding: 10px;
-`;
-
 interface ChatMessage {
   sender: "user" | "bot";
   content: string;
@@ -39,7 +17,7 @@ const ChatBot: React.FC = () => {
     setUserInput(event.target.value);
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     if (userInput.trim() === "") {
       return; // 빈 입력 방지
     }
@@ -48,6 +26,28 @@ const ChatBot: React.FC = () => {
       content: userInput,
     };
     setMessages(messages.concat(newUserMessage));
+
+    try {
+      const response = await fetch("http://localhost:8000/chat/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: userInput }),
+      });
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+      const data = await response.json();
+      const botReply: ChatMessage = {
+        sender: "bot",
+        content: data.choices[0].message.content,
+      };
+      setMessages((prevMessages) => [...prevMessages, botReply]);
+    } catch (error) {
+      console.error("ChatGPT API Error:", error);
+    }
+
     setUserInput(""); // 입력 필드 초기화
   };
 
@@ -69,3 +69,25 @@ const ChatBot: React.FC = () => {
 };
 
 export default ChatBot;
+
+const ChatContainer = styled.div`
+  // 채팅 컨테이너 스타일
+`;
+
+const Message = styled.div<{ sender: string }>`
+  // 메시지 스타일 (sender에 따라 다름)
+`;
+
+const InputArea = styled.div`
+  margin-top: 20px;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  margin-right: 10px;
+  width: 70%;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+`;
